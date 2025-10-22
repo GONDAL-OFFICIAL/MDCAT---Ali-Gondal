192697082010010e84bfb9915c5a04f7e18af428
GONDAL_OFFICIAL <gondalofficial307@gmail.com>
2025-10-22 21:05:02 +0500

    Set correct base and add deploy scripts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, ".", "");
   return {
-    base: "/modern-quiz/", // <-- add this line for GitHub Pages
+    base: "/MDCAT---Ali-Gondal/", // <-- add this line for GitHub Pages
     server: {
       port: 3000,
       host: "0.0.0.0",
